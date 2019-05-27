8936b8a383faa4c0bf8420bf327d120233c264f5
carmenpmoreno <carmenp.moreno@gmail.com>
2019-05-27 17:22:11 +0200

    build docs

---
+++ docs/precache-manifest.b7f92a626c591cb11b7592bbc0da5802.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cbe9c1ac2b0494427af8dca936f3e34",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9e5fda7885a62d569553",
+    "url": "./static/css/main.92ab3167.chunk.css"
+  },
+  {
+    "revision": "d3ccdf2b09ba9a7d8c92",
+    "url": "./static/js/2.e096568a.chunk.js"
+  },
+  {
+    "revision": "9e5fda7885a62d569553",
+    "url": "./static/js/main.417e6f4b.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
--- src/components/App.js
+++ src/components/App.js
@@ -31,7 +31,7 @@ class App extends React.Component {
   }
 
   getCharacters() {
-    fetch('http://hp-api.herokuapp.com/api/characters')
+    fetch('https://hp-api.herokuapp.com/api/characters')
       .then(response => response.json())
       .then(data => {
 
